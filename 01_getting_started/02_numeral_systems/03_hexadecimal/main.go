644cee8d6c7a86692eca0fa47fdc7ca8140df86d
Dylan van Gils <dylanvgils@users.noreply.github.com>
2017-02-10 10:41:21 +0100

    Hello world and numeral systems

---
+++ 01_getting_started/02_numeral_systems/03_hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// # Addes indentification prefex eg. 0x to hexadecimal number
+	fmt.Printf("Decimal: %d - Binary: %b - Hexadecimal: %#x\n", 42, 42, 42)
+}
