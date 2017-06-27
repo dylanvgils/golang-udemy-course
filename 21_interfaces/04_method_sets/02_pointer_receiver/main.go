8afbb596679fc392a33b2ee50039fca148f75e55
Dylan van Gils <dylanvangils@gmail.com>
2017-06-27 17:38:38 +0200

    Method sets

---
+++ 21_interfaces/04_method_sets/02_pointer_receiver/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func (c *circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func info(s shape) {
+	fmt.Println("area", s.area())
+}
+
+func main() {
+	c := circle{5}
+	// info(c) // Does not work
+	info(&c)
+}
+
+/*
+	Receivers       Values
+	-------------------------
+	(t T)           T and *T
+	(t *T)          *T
+*/
